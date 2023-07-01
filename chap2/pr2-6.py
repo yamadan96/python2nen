b968c51636a6ab15284577c6194da30ac2673a68
yamadan96 <yuto20010906@gmail.com>
2023-07-01 16:26:00 +0900

    Add

--- chap2/pr2-6.py
+++ chap2/pr2-6.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import urllib
 
 # Webページを取得して解析する
 load_url = "https://www.ymori.com/books/python2nen/test2.html"
@@ -8,8 +7,5 @@ html = requests.get(load_url)
 soup = BeautifulSoup(html.content, "html.parser")
 
 
-for element in soup.find_all("a"):
+for element in topic.find_all("a"):
     print(element.text)
-    url = element.get("href")
-    link_url = urllib.parse.urljoin(load_url, url)
-    print(link_url)
